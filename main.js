7666ea90938b78049f86d6380f520dba17e1a189
Junwon_Park <upjw0429@gmail.com>
2021-12-31 00:32:59 +0900

    add toggle switch done

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+let toggle = document.querySelector('.nav__toggleBtn');
+let menu = document.querySelector('.menu__list');
+let icon = document.querySelector('.icon__list');
+
+console.log(toggle);
+toggle.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  icon.classList.toggle('active');
+});
